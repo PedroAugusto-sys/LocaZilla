5ae3f3757f0413a37be3b42269f2d004145462db
PedroAugusto-sys <pedrocaandrade01@gmail.com>
2022-11-02 12:26:19 -0300

    up

---
+++ LocaZilla/src/com/LocaZilla/control/marca/IMarcaControle.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.LocaZilla.control.marca;
+
+import com.LocaZilla.model.marca.Marca;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pedro
+ */
+public interface IMarcaControle {
+    void incluir(Marca objeto)throws Exception;
+    void alterar(Marca objeto)throws Exception;
+    ArrayList<Marca> listagem()throws Exception;
+    Marca  buscar(int obj)throws Exception;
+    Marca  buscar(String descricao)throws Exception;
+    
+}
---
+++ LocaZilla/src/com/LocaZilla/model/operador/Operador.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.LocaZilla.model.operador;
+
+/**
+ *
+ * @author pedro
+ */
+public class Operador implements Comparable<Operador>{
+    private int idOperador = 0;
+    private String nomeOperador = "";
+    private String senhaOperador = "";  
+    private String urlOperador = "";
+
+    public Operador() {
+    }
+
+    
+     public Operador(int idOperador, String nomeOperador, String senhaOperador, String urlOperador) {
+         this.idOperador = idOperador;
+         this.nomeOperador = nomeOperador;
+         this.senhaOperador = senhaOperador;
+         this.urlOperador = urlOperador;
+    }
+
+    
+    public int getIdOperador() {
+        return idOperador;
+    }
+
+    public void setIdOperador(int idOperador) {
+        this.idOperador = idOperador;
+    }
+
+    public String getNomeOperador() {
+        return nomeOperador;
+    }
+
+    public void setNomeOperador(String nomeOperador) {
+        this.nomeOperador = nomeOperador;
+    }
+
+    public String getSenhaOperador() {
+        return senhaOperador;
+    }
+
+    public void setSenhaOperador(String senhaOperador) {
+        this.senhaOperador = senhaOperador;
+    }
+
+    public String getUrlOperador() {
+        return urlOperador;
+    }
+
+    public void setUrlOperador(String urlOperador) {
+        this.urlOperador = urlOperador;
+    }
+    
+   
+    @Override
+    public String toString(){
+        return idOperador+";"+nomeOperador+";"+senhaOperador+";"+urlOperador;
+    }
+    
+     @Override
+    public int compareTo(Operador id) {
+     if(this.idOperador == id.getIdOperador()) return (this.idOperador++);
+     else return(this.idOperador - id.getIdOperador());
+    }
+    
+    
+}
