114dd5df3168c9ff39c2099ed6a730818c0e042e
PedroAugusto-sys <pedrocaandrade01@gmail.com>
2022-11-11 17:08:04 -0300

    up

--- LocaZilla/src/com/LocaZilla/control/modelo/ModeloControle.java
+++ LocaZilla/src/com/LocaZilla/control/modelo/ModeloControle.java
@@ -15,24 +15,23 @@ import java.util.Iterator;
  *
  * @author pedro
  */
-public class ModeloControle implements IModeloControle{
-    
-    
-            IModeloDAO modeloPersistencia = null;
-            
-            public ModeloControle(){
-                
-            this.modeloPersistencia = new ModeloDAO();
-       
-            }
-            
-    private boolean buscarModelo (String descricao)throws Exception{
-            try {
+public class ModeloControle implements IModeloControle {
+
+    IModeloDAO modeloPersistencia = null;
+
+    public ModeloControle() {
+
+        this.modeloPersistencia = new ModeloDAO();
+
+    }
+
+    private boolean buscarModelo(String descricao) throws Exception {
+        try {
             ArrayList<Modelo> listagem = modeloPersistencia.listagem();
             Iterator<Modelo> lista = listagem.iterator();
-            while(lista.hasNext()){
-                 Modelo aux = lista.next();
-                if(aux.getDescricao().equalsIgnoreCase(descricao)){
+            while (lista.hasNext()) {
+                Modelo aux = lista.next();
+                if (aux.getDescricao().equalsIgnoreCase(descricao)) {
                     return true;
                 }
             }
@@ -40,79 +39,77 @@ public class ModeloControle implements IModeloControle{
         } catch (Exception erro) {
             throw erro;
         }
-        }
+
+    }
+
     @Override
     public void incluirModelo(Modelo objeto) throws Exception {
-       if(buscarModelo(objeto.getDescricao())){
+        if (buscarModelo(objeto.getDescricao())) {
             throw new Exception("Modelo já foi cadastrado.");
         }
-       if(buscarModelo(objeto.getUrl())){
-           throw new Exception("Foto já foi cadastrada.");
-       }
+        if (buscarModelo(objeto.getUrl())) {
+            throw new Exception("Foto já foi cadastrada.");
+        }
         modeloPersistencia.incluir(objeto);
     }
 
     @Override
     public void alterarModelo(Modelo objeto) throws Exception {
-            if(objeto.getDescricao().equals("") || objeto.getUrl().equals("")){
+        if (objeto.getDescricao().equals("") || objeto.getUrl().equals("")) {
             throw new Exception("Preencha os campos corretamente.");
-        }else{
-            
-        modeloPersistencia.alterar(objeto);
-    
-            }
+        } else {
+
+            modeloPersistencia.alterar(objeto);
+
+        }
     }
 
     @Override
     public ArrayList<Modelo> listagemModelo() throws Exception {
-         return modeloPersistencia.listagem();
+        return modeloPersistencia.listagem();
     }
-    
-    
+
     //EXCESSÕES==================
-    
     @Override
-    public void mesmaFoto(Modelo url)throws Exception{
+    public void mesmaFoto(Modelo url) throws Exception {
         ArrayList<Modelo> lista = listagemModelo();
-        
-        for(int i = 0; i < lista.size(); i++){
-            if(url.getUrl().equals(lista.get(i).getUrl())){
-            throw new Exception("Modelo com esta foto já foi adicionado");
-        }
-            
+
+        for (int i = 0; i < lista.size(); i++) {
+            if (url.getUrl().equals(lista.get(i).getUrl())) {
+                throw new Exception("Modelo com esta foto já foi adicionado");
+            }
+
         }
     }
-    
-     @Override
-    public void semFoto(Modelo url)throws Exception{
-            if(url.getUrl().equals("")){
-                throw new Exception("Selecione a imagem do modelo para continuar");
+
+    @Override
+    public void semFoto(Modelo url) throws Exception {
+        if (url.getUrl().equals("")) {
+            throw new Exception("Selecione a imagem do modelo para continuar");
         }
 
     }
-    
-    
-     @Override
+
+    @Override
     public void vazio(Modelo desc) throws Exception {
-        if(desc.getDescricao().equals("")){
-             throw new Exception("Nenhum modelo sendo cadastrado");
+        if (desc.getDescricao().equals("")) {
+            throw new Exception("Nenhum modelo sendo cadastrado");
         }
-        
+
     }
-    
-        public void conferirBancoDeDados(){
-        
+
+    public void conferirBancoDeDados() {
+
         try {
             File bancodedados = new File("./src/com/LocaZilla/Dados/modelo/Modelo.txt");
-            if(!bancodedados.exists()){
-               
+            if (!bancodedados.exists()) {
+
                 bancodedados.createNewFile();
-                
+
             }
         } catch (Exception e) {
         }
-        
-        
+
     }
-    
+
 }
