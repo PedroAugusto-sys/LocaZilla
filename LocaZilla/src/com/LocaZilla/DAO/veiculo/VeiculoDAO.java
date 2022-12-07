59b21665703849e369a7c6d41289a186ac7188ed
Hugodias95 <hugo.dias222@gmail.com>
2022-12-07 19:36:20 -0300

    VeiculoDAO vazio

---
+++ LocaZilla/src/com/LocaZilla/DAO/veiculo/VeiculoDAO.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.LocaZilla.DAO.veiculo;
+
+import com.LocaZilla.model.veiculo.Veiculo;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hugo_
+ */
+public class VeiculoDAO implements IVeiculoDAO {
+
+    @Override
+    public void incluir(Veiculo objeto) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void alterar(Veiculo objeto) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<Veiculo> listagem() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String selecionarVeiculos(int id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
